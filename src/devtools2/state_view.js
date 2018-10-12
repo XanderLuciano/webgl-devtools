b7186f62572de589e41b911231f73b08436f33fa
Xander Luciano <xanderluciano@gmail.com>
2018-10-12 16:36:42 -0700

    Improve state_view syntax

--- src/devtools2/state_view.js
+++ src/devtools2/state_view.js
@@ -9,90 +9,113 @@ isEditable = [
 ];
 
 define(["messages"], function (Messages) {
-    var StateView = React.createClass({
-        getInitialState: function() {
-            return {enabled: false, stateVars: {}, enumOptions: {}};
-        },
-        componentWillMount: function() {
-            Messages.connection.onMessage.addListener(function(msg) {
-                if (msg.source != "content") {return;}
+    return React.createClass({
 
-                if (msg.type == messageType.STATE_VARS) {
-                    var stateVars = JSON.parse(msg.data.stateVars);
-                    var enumOptions = JSON.parse(msg.data.enumOptions);
-                    this.setState({stateVars: stateVars, enumOptions: enumOptions});
-                }
-            }.bind(this));
+        getInitialState: function() {
+            return {
+                enabled     : false,
+                stateVars   : {},
+                enumOptions : {},
+            };
         },
+
+        componentWillMount: function() {
+            Messages.connection.onMessage.addListener( (msg) => {
+                if (msg.source !== "content") return;
+
+                if (msg.type === messageType.STATE_VARS) {
+                    const stateVars   = JSON.parse(msg.data.stateVars);
+                    const enumOptions = JSON.parse(msg.data.enumOptions);
+
+                    this.setState({
+                        stateVars   : stateVars,
+                        enumOptions : enumOptions,
+                    });
+                }
+            });
+        },
+
         getVariableColumn: function(data) {
-            result = [];
-            for (var i = 0; i < data.length; i++) {
-                var className = "profile-table-column-element";
-                if (i == 0) {
+            const result = [];
+            for (let i = 0; i < data.length; i++) {
+                let className = "profile-table-column-element";
+                if (i === 0) {
                     className += " profile-table-column-element-header";
                 }
-                el = <div className={className}>{data[i]}</div>
+                const el = <div className={className}>{data[i]}</div>;
                 result.push(el);
             }
             return <div className="profile-table-column">
                 {result}
             </div>;
         },
+
         sendBoolUpdate: function(variableName, value) {
             Messages.sendMessage(this.props.activeContext, messageType.STATE_VARS, {
-                type: "bool",
-                variable: variableName,
-                enable: value
+                type     : "bool",
+                variable : variableName,
+                enable   : value,
             });
         },
+
         sendEnumUpdate: function(variableName, value) {
             Messages.sendMessage(this.props.activeContext, messageType.STATE_VARS, {
-                type: "enum",
-                variable: variableName,
-                value: value
+                type     : "enum",
+                variable : variableName,
+                value    : value,
             });
         },
+
         sendNumberUpdate: function(variableName, value) {
             Messages.sendMessage(this.props.activeContext, messageType.STATE_VARS, {
-                type: "num",
-                variable: variableName,
-                value: Number(value)
+                type     : "num",
+                variable : variableName,
+                value    : Number(value),
             });
         },
+
         boolChange: function(variableName, e) {
-            var value = e.target.checked;
+            const value = e.target.checked;
             this.sendBoolUpdate(variableName, value);
         },
+
         enumChange: function(variableName, e) {
-            var value  = e.target.value;
+            const value  = e.target.value;
             this.sendEnumUpdate(variableName, value);
         },
+
         numberChange: function(variableName, e) {
-            var value = e.target.value;
+            const value = e.target.value;
             this.sendNumberUpdate(variableName, value);
         },
+
         getStateColumn: function(data) {
-            result = [];
-            for (var i = 0; i < data.length; i++) {
-                var className = "profile-table-column-element";
-                var subEl;
-                if (i == 0) {
+            const result = [];
+            for (let i = 0; i < data.length; i++) {
+                let className = "profile-table-column-element";
+                let subEl;
+
+                if (i === 0) {
                     className += " profile-table-column-element-header";
                     subEl = "State";
                 } else {
-                    var name = data[i - 1];
-                    var type = this.state.stateVars[name].type;
-                    var value = this.state.stateVars[name].value;
-                    if (type == "bool") {
-                        subEl = <input
-                            type="checkbox"
-                            checked={value}
-                            onClick={this.boolChange.bind(this, name)}/>;
-                    } else if (type == "enum") {
+
+                    const name  = data[i - 1];
+                    const type  = this.state.stateVars[name].type;
+                    const value = this.state.stateVars[name].value;
+
+                    if (type === "bool") {
+
+                        subEl = <input type="checkbox"
+                                       checked={value}
+                                       onClick={this.boolChange.bind(this, name)}/>;
+
+                    } else if (type === "enum") {
+
                         if (this.state.enumOptions[name] && this.state.enumOptions[name].length > 1) {
-                            var options = this.state.enumOptions[name];
-                            var optionEls = []
-                            for (var j=0; j<options.length; j++) {
+                            const options = this.state.enumOptions[name];
+                            const optionEls = [];
+                            for (let j=0; j<options.length; j++) {
                                 optionEls.push(
                                     <option value={options[j]}>{options[j]}</option>
                                 );
@@ -101,7 +124,9 @@ define(["messages"], function (Messages) {
                         } else {
                             subEl = <div>{value}</div>; // Setting not supported
                         }
-                    } else if (type == "number") {
+
+                    } else if (type === "number") {
+
                         if (isEditable.indexOf(name) > 0) {
                             subEl = <input type="number"
                                            defaultValue={value}
@@ -111,43 +136,49 @@ define(["messages"], function (Messages) {
                         }
                     }
                 }
-                el = <div className={className}>{subEl}</div>
+                const el = <div className={className}>{subEl}</div>;
                 result.push(el);
             }
             return <div className="profile-table-column">
                 {result}
             </div>;
         },
-        getStateColumns: function(data) {
-            var columns = [];
 
-            var vList = Object.keys(this.state.stateVars).sort();
-            var variables = ["Variable"].concat(vList);
+        getStateColumns: function(data) {
+            const columns = [];
+
+            const vList = Object.keys(this.state.stateVars).sort();
+            const variables = ["Variable"].concat(vList);
 
             columns.push(this.getVariableColumn(variables));
             columns.push(this.getStateColumn(vList));
             return columns;
         },
+
         getTabs: function() {
-            el = <div className="split-view-table-element split-view-table-element-selected">
+            return <div className="split-view-table-element split-view-table-element-selected">
                 <div className="split-view-table-element-text">
                     State Editor
                 </div>
-            </div>
-            return el;
+            </div>;
         },
+
         toggleStateEditor: function() {
-            var enabled = this.refs.stateEditor.checked;
-            var data = {"enabled": enabled};
+            let enabled = this.refs.stateEditor.checked;
+            let data = {"enabled": enabled};
+
             Messages.sendMessage(this.props.activeContext, messageType.TOGGLE_STATE_VARS, data);
 
             if (enabled) {
                 Messages.sendMessage(this.props.activeContext, messageType.STATE_VARS, "getStateVariables");
             }
+
             this.setState(data);
         },
+
         render: function() {
-            var table = null;
+            let table = null;
+
             if (this.state.enabled) {
                 table = <div className="profile-table">
                     {this.getStateColumns()}
@@ -172,5 +203,4 @@ define(["messages"], function (Messages) {
             </div>
         }
     });
-    return StateView;
 });
